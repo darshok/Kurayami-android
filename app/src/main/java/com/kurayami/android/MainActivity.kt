f06d7a1f24b36a6bb033f20d53ee32ea67dadca9
Daniel Fenollar <daniel.fo.96@gmail.com>
2024-08-07 12:53:27 +0200

    Merge into master (#4)
    * Create README.md
    
    * feat: upgrade android application version, kotlin and increased target sdk (#1)
    
    * Create LICENSE (#2)
    
    * feat: restart project with compose (#3)
    
    Co-authored-by: dfenolla <dfenolla@mercadona.es>
    
    ---------
    
    Co-authored-by: dfenolla <dfenolla@mercadona.es>

--- app/src/main/java/com/kurayami/android/MainActivity.kt
+++ app/src/main/java/com/kurayami/android/MainActivity.kt
@@ -1,28 +1,29 @@
-package com.kurayami.player
+package com.kurayami.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.kurayami.player.ui.theme.KurayamiPlayerTheme
+import com.kurayami.android.ui.theme.KurayamiAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            KurayamiPlayerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Greeting("Android")
+            KurayamiAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -33,14 +34,14 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier,
+        modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KurayamiPlayerTheme {
+    KurayamiAndroidTheme {
         Greeting("Android")
     }
 }
