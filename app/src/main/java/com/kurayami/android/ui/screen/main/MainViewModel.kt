17ee3791449808303d0137dbf19d4b2510474790
Daniel Fenollar <daniel.fo.96@gmail.com>
2024-12-11 15:00:50 +0100

    feat: added image to top list and shimmer effect (#10)

--- app/src/main/java/com/kurayami/android/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/kurayami/android/ui/screen/main/MainViewModel.kt
@@ -3,31 +3,42 @@ package com.kurayami.android.ui.screen.main
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.kurayami.data.repository.LoginRepository
 import com.kurayami.data.repository.MediaRepository
 import com.kurayami.data.repository.PreferencesDataStoreRepository
 import com.kurayami.data.type.MediaSort
 import com.kurayami.data.type.MediaType
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+data class MediaTopChartUiState(
+    val isLoading: Boolean = true,
+    val hasNextPage: Boolean = false,
+    val currentPage: Int = 1,
+)
+
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    preferencesDataStoreRepository: PreferencesDataStoreRepository,
+    mediaRepository: MediaRepository,
     private val loginRepository: LoginRepository,
-    private val preferencesDataStoreRepository: PreferencesDataStoreRepository,
-    private val mediaRepository: MediaRepository,
-) :
-    ViewModel(), MainActions {
+) : ViewModel(), MainActions {
 
     val isUserLoggedIn = preferencesDataStoreRepository.getAccessToken().map { it != null }
 
-    private val _uiState by lazy { getUiState() }
+    private val _uiState by lazy { MutableStateFlow(MediaTopChartUiState()) }
     val uiState: StateFlow<MediaTopChartUiState> by lazy { _uiState.asStateFlow() }
+
+    val topChartFlow by lazy {
+        mediaRepository.getTopCharts(
+            25,
+            MediaType.ANIME,
+            listOf(MediaSort.SCORE_DESC)
+        ).onCompletion { _uiState.update { it.copy(isLoading = false) } }.cachedIn(viewModelScope)
+    }
 
     override fun manageIntentData(data: Uri?) {
         data?.let { dataUri ->
@@ -45,15 +56,6 @@ class MainViewModel @Inject constructor(
             loginRepository.manageLogout()
         }
     }
-
-    private fun getUiState() =
-        MutableStateFlow(MediaTopChartUiState(getAnimeChart()))
-
-    private fun getAnimeChart() = mediaRepository.getTopCharts(
-        10,
-        MediaType.ANIME,
-        listOf(MediaSort.SCORE_DESC)
-    )
 
     private fun isAuthenticationUri(data: Uri?) =
         data?.scheme == "kurayami"
